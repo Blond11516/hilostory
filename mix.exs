ced29274747baaf8024235b9b3ad989028761e3f
Étienne Lévesque <contact@etiennel.dev>
2025-11-08 10:43:34 -0500

    update elixir dependencies

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Hilostory.MixProject do
   defp deps do
     [
       {:bandit, "1.8.0"},
-      {:castore, "1.0.15"},
+      {:castore, "1.0.16"},
       {:cloak, "1.1.4"},
       {:cloak_ecto, "1.3.0"},
       {:ecto_sql, "3.13.2"},
@@ -44,10 +44,10 @@ defmodule Hilostory.MixProject do
       {:joken, "2.6.2"},
       {:joken_jwks, "1.7.0"},
       {:phoenix, "1.8.1"},
-      {:phoenix_ecto, "4.6.5"},
+      {:phoenix_ecto, "4.7.0"},
       {:phoenix_html, "4.3.0"},
       {:phoenix_live_dashboard, "0.8.7"},
-      {:phoenix_live_view, "1.1.16"},
+      {:phoenix_live_view, "1.1.17"},
       {:postgrex, "0.21.1"},
       {:req, "0.5.15"},
       {:telemetry_metrics, "1.1.0"},
@@ -57,11 +57,11 @@ defmodule Hilostory.MixProject do
       {:typed_struct, "0.3.0"},
       {:tz, "0.28.1"},
       {:websockex, "0.4.3"},
-      {:zoi, "0.9.0-rc.0"},
+      {:zoi, "0.9.1"},
       {:phoenix_live_reload, "1.6.1", only: :dev},
-      {:igniter, "0.6.30", only: :dev},
+      {:igniter, "0.7.0", only: :dev},
       {:styler, "1.9.1", only: [:dev, :test], runtime: false},
-      {:dialyxir, "1.4.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "1.4.7", only: [:dev, :test], runtime: false},
       {:lazy_html, "0.1.8", only: :test}
     ]
   end
